51e0b6f69c7879365dee989293476d94bd226ccf
Srinu Madhav <43718077+srinumadhavv@users.noreply.github.com>
2022-02-07 17:25:16 +0530

    uncommented conditions

--- app/routers/coupons.py
+++ app/routers/coupons.py
@@ -28,8 +28,8 @@ def create_user( coupon : schemas.CouponCreate, db : Session = Depends(get_db)):
     current_time=calendar.timegm(time.gmtime(0))
     if coupon_name and coupon_name.expiry_date > current_time:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"Coupon already exists in database")
-    # if coupon_type.lower() not in ["percentage","fixed"] :
-    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=f"type must be either percentage or fixed")
+    if coupon_type.lower() not in ["percentage","fixed"] :
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=f"type must be either percentage or fixed")
     if start_date > expiry_date :
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=f"expiry date must be greater than or equal to Start Date")
     if coupon_type == "fixed" and discount > min_amount:
